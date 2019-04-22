6a01d239900ed05f089d5a06c1ef91510b9aa521
Prashant Pandey <prash22face@gmail.com>
2019-04-22 23:31:58 +0530

    initial commit

---
+++ routes/database.js
@@ -0,0 +1,15 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'prashant',
+        'password': 'root',
+        'port': '3306'
+    },
+    'database': 'proptrading',
+    'tables': {
+        'users_table': 'proptrading_users',
+        'subscription': 'proptrading_subscription_list',
+        'contact': 'proptrading_contact_me'
+    }
+};
